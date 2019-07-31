625d5651d78896307fa98b586f2c47828a00eec8
Ubuntu <ubuntu@ip-172-31-43-41.ec2.internal>
2019-07-31 17:26:24 +0000

    started setting up AWS S3 Buckets to store static files, however from Codeinstitute they say our account doesn't work so I commented out settings added in settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -135,6 +135,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+###############
+# I didn't install python packages for AWS S3 Buckets as they don't 
+# work with educational account
+# AWS_S3_OBJECT_PARAMETERS = {
+#     'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+#     'CacheControl': 'max-age=94608000',
+# }
+# AWS_STORAGE_BUCKET_NAME = 'django-ecommerce-bucket'
+# AWS_S3_REGION_NAME = 'eu-west-1'
+# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
+
+
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
