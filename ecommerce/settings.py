d9d8dd66631a5c8382ffe6418a8f2dad694a619b
Ubuntu <ubuntu@ip-172-31-43-41.ec2.internal>
2019-07-31 17:42:11 +0000

    changed storage in AWS S3 Bucket so media files are stored in media folder and static files in static folder. Most commented out as issue with AWS Educational account

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -149,7 +149,11 @@ USE_TZ = True
 
 # AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
+# STATICFILES_LOCATION = 'static'
+# STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+
+# MEDIAFILES_LOCATION = 'media'
+# DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 
 
